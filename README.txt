6dcac361ddb92907a48ef5a0b4c21e8501cf5f79
Claudio Fritsche <claudio.fritsche@epfl.ch>
2017-04-20 23:50:31 +0200

    Everything is set up to work only with the macbook OSX and not use the vm anymore

---
+++ README.txt
@@ -0,0 +1,3 @@
+This directory contains the software for the robot sticky turtle. Please contact 
+claudio.fritsche@epfl.ch if you’ve got any questions.
+
---
+++ software/bottle_detection/bottle_approach.py
@@ -0,0 +1,8 @@
+# This script does the bottle approach getting the information about the bottle location from bottle_detection.
+
+# imports libraries
+import cv2
+
+# import own modules
+import bottle_detection
+print('testing this')
